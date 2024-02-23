276ddbb8faf0f72aa17dd980219d3cf1fba5b851
Chloe Bishop <chloebee123@gmail.com>
2024-02-23 09:21:41 +0000

    updated arrays to include a song by the strokes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const bands = ["The Stone Roses", "Nirvana", "Arctic Monkeys", "The Cure", "Foo Fighters", "Motorhead", "Paramore", "Talking Heads", "Green Day", "Tame Impala", "The White Stripes", "Radiohead", "Rage Against the Machine", "The Smiths", "Joy Division", "The Clash"];
-const singers = ["Kurt Cobain", "Ian Brown", "Alex Turner", "Hayley Williams", "Dave Grohl", "David Byrne", "Billie Joe Armstrong", "Kevin Parker", "Jack White", "Thom Yorke", "Zack de la Rocha", "Morrissey", "Ian Curtis", "Joe Strummer"];
-const songs = ["Fools Gold", "Brick by Boring Brick", "Teddy Picker", "A Forest", "Everlong", "Ace of Spades", "Psycho Killer", "Elephant", "Seven Nation Army", "Creep", "Killing in the Name", "This Charming Man", "Love Will Tear Us Apart", "Rock the Casbah"];
+const bands = ["The Stone Roses", "Nirvana", "Arctic Monkeys", "The Cure", "Foo Fighters", "Motorhead", "Paramore", "Talking Heads", "Green Day", "Tame Impala", "The White Stripes", "Radiohead", "Rage Against the Machine", "The Smiths", "Joy Division", "The Clash", "The Strokes"];
+const singers = ["Kurt Cobain", "Ian Brown", "Alex Turner", "Robert Smith", "Hayley Williams", "Dave Grohl", "David Byrne", "Billie Joe Armstrong", "Kevin Parker", "Jack White", "Thom Yorke", "Zack de la Rocha", "Morrissey", "Ian Curtis", "Joe Strummer", "Julian Casablancas"];
+const songs = ["Fools Gold", "Brick by Boring Brick", "Teddy Picker", "A Forest", "Everlong", "Ace of Spades", "Psycho Killer", "Elephant", "Seven Nation Army", "Creep", "Killing in the Name", "This Charming Man", "Love Will Tear Us Apart", "Rock the Casbah", "Last Nite"];
 
 let bandIndex = Math.floor(Math.random() * bands.length);
 let singerIndex = Math.floor(Math.random() * singers.length);
